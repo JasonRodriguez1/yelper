d748ce92c25bd7fe34df2523acff745ba61b4ec8
Jason Rodriguez <jrodriguez.actual@gmail.com>
2014-09-24 11:48:40 -0700

    Addded image field to db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140918161736) do
+ActiveRecord::Schema.define(version: 20140924072143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20140918161736) do
     t.string   "website"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
 end
