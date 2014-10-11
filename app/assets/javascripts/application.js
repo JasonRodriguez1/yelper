3803dcd9928c1841d88b842cc3db3ac05371033c
Jason Rodriguez <jrodriguez.actual@gmail.com>
2014-10-11 12:59:28 -0700

    Removed js from file, it isn't supposed to be there

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,13 +19,3 @@
 $(function() {
   $(document).foundation();
 });
-$(document).ready(function(){
-  // Hide after div by default
-  $('#after-review').hide();
-  $('.instruction #after-review').hide();
-  $('.review-create .button').click(function(){
-      $('#before-review').hide();
-      $('#after-review').show();
-      $('.instruction #after-review').show();
-  });
-});
