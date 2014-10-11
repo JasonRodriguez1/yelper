efeeec84a8a2c7b2605cc2bbb4214b9e6619d2c4
Jason Rodriguez <jrodriguez.actual@gmail.com>
2014-10-11 16:32:26 -0700

    Work in progress

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,10 +1,11 @@
-$(document).ready(function(){
-  // Hide after div by default
-  $('#after-review').hide();
-  $('.instruction #after-review').hide();
-  $('.review-create .button').click(function(){
+$(document).ready(function() {
+    // Hide after div by default
+    $('#after-review').hide();
+    $('.instruction #after-review').hide();
+    $( ".review-create .button" ).click(function() {
+      alert("I am an alert box!");
       $('#before-review').hide();
       $('#after-review').show();
       $('.instruction #after-review').show();
-  });
+    });
 });
