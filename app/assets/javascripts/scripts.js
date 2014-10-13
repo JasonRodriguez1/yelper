1775ac64a0b5a78cee98a9a4bf483b5e83899a9b
Jason Rodriguez <jrodriguez.actual@gmail.com>
2014-10-13 13:16:17 -0700

    Moved event handler to same page as view where divs were located

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
     // Hide after div by default
     $('#after-review').hide();
     $('.instruction #after-review').hide();
-    $( ".review-create .button" ).click(function() {
-      alert("I am an alert box!");
+    $("h4 a").on("click", function() {
       $('#before-review').hide();
+      $('#before-review h4').hide();
       $('#after-review').show();
       $('.instruction #after-review').show();
     });
