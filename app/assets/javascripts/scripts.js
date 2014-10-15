2ba91d4cfb6a8255bba737aa444947ed3fb8be39
Jason Rodriguez <jrodriguez.actual@gmail.com>
2014-10-14 19:36:21 -0700

    Added JS for tooltips

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
-    // Hide after div by default
-    $('#after-review').hide();
-    $('.instruction #after-review').hide();
-    $("h4 a").on("click", function() {
-      $('#before-review').hide();
-      $('#before-review h4').hide();
-      $('#after-review').show();
-      $('.instruction #after-review').show();
-    });
+
 });
+// jQuery UI Tooltip styling
+$( document ).tooltip({
+      track: true,
+      using: function( position, feedback ) {
+        $( this ).css( position );
+        $( "<div>" )
+          .addClass( "arrow" )
+          .addClass( feedback.vertical )
+          .addClass( feedback.horizontal )
+          .appendTo( this );
+      }
+  });
