0c43dd4b34d39ff2814c55058ebb65ec172651cf
Jason Rodriguez <jrodriguez.actual@gmail.com>
2014-10-15 01:35:14 -0700

    Removed mouse tracking from tooltip

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -3,13 +3,15 @@ $(document).ready(function() {
 });
 // jQuery UI Tooltip styling
 $( document ).tooltip({
-      // track: true,
-      using: function( position, feedback ) {
-        $( this ).css( position );
-        $( "<div>" )
-          .addClass( "arrow" )
-          .addClass( feedback.vertical )
-          .addClass( feedback.horizontal )
-          .appendTo( this );
-      }
+      track: false,
+      my: "left top",
+      at: "left bottom",
+      // using: function( position, feedback ) {
+      //   $( this ).css( position );
+      //   $( "<div>" )
+      //     .addClass( "arrow" )
+      //     .addClass( feedback.vertical )
+      //     .addClass( feedback.horizontal )
+      //     .appendTo( this );
+      // }
   });
